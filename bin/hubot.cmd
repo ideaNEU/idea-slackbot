32e272dc2db4c8ced202f97a092091893919ba5c
Drew <drew@pomerleaudesign.com>
2016-12-04 18:06:17 -0500

    update for aws

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,7 +1,5 @@
 @echo off
 
-call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
-
-node_modules\.bin\hubot.cmd --name "ideabot" %* 
+node_modules\.bin\hubot.cmd --name "ideabot" %*
